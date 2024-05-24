bf95dd9eac5c5c18db2c580220ec59bee80b87dd
Bảo Ngọc <baokhuong6996@gmail.com>
2024-05-24 15:51:54 +0700

    New Database

--- KeiTaiZonePHP.sql
+++ KeiTaiZonePHP.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost:8889
--- Thời gian đã tạo: Th5 20, 2024 lúc 08:00 AM
+-- Thời gian đã tạo: Th5 24, 2024 lúc 08:50 AM
 -- Phiên bản máy phục vụ: 5.7.39
 -- Phiên bản PHP: 8.2.0
 
@@ -104,6 +104,8 @@ CREATE TABLE `products` (
   `sku` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `featured` tinyint(1) NOT NULL,
   `tag` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Views` int(11) NOT NULL,
+  `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -112,11 +114,12 @@ CREATE TABLE `products` (
 -- Đang đổ dữ liệu cho bảng `products`
 --
 
-INSERT INTO `products` (`id`, `brand_id`, `product_category_id`, `name`, `description`, `content`, `price`, `qty`, `discount`, `weight`, `sku`, `featured`, `tag`, `created_at`, `updated_at`) VALUES
-(3, 2, 1, 'Iphone 13 128GB', 'Về kích thước, iPhone 13 sẽ có 4 phiên bản khác nhau và kích thước không đổi so với series iPhone 12 hiện tại. Nếu iPhone 12 có sự thay đổi trong thiết kế từ góc cạnh bo tròn (Thiết kế được duy trì từ thời iPhone 6 đến iPhone 11 Pro Max) sang thiết kế vuông vắn (đã từng có mặt trên iPhone 4 đến iPhone 5S, SE).\r\n\r\nThì trên điện thoại iPhone 13 vẫn được duy trì một thiết kế tương tự. Máy vẫn có phiên bản khung viền thép, một số phiên bản khung nhôm cùng mặt lưng kính. Tương tự năm ngoái, Apple cũng sẽ cho ra mắt 4 phiên bản là iPhone 13, 13 mini, 13 Pro và 13 Pro Max.', NULL, 13590000, 15, NULL, NULL, NULL, 0, NULL, NULL, NULL),
-(4, 2, 1, 'iPhone 15 Pro Max 256GB', 'Hiệu năng mạnh mẽ cùng chipset A17 Pro tiên tiến \r\nSản phẩm trang bị chip A17 Pro được sản xuất trên tiến trình 3nm, mang lại bước cải tiến đột phá. Nhờ những cải tiến về thiết kế và kiến trúc vi mô, CPU mới nhanh hơn đến 10%, hỗ trợ tính năng như Giọng nói cá nhân hay tự động sửa lỗi chuyên nghiệp. Không chỉ vậy, GPU nhanh hơn đến 20% với thiết kế 6 lõi mới nhằm nâng cao hiệu suất và tiết kiệm năng lượng.\r\niPhone 15 Pro đã có một bước tiến đáng kể với công nghệ dò tia tăng tốc phần cứng, với tốc độ nhanh hơn gấp 4 lần so với các phiên bản trước đây dùng công nghệ dò tia dựa trên phần mềm. Kết quả của sự cải tiến này là trải nghiệm đồ họa trên thiết bị trở nên mượt mà hơn, đặc biệt là trải nghiệm khi chơi game và sử dụng ứng dụng thực tế ảo (AR) sẽ trở nên sống động hơn. Người dùng sẽ có trải nghiệm chiến game mượt mà hơn với: Resident Evil 4, Seadth Stranding, Resident Evil Village hay Assassin\'s Creed Mirage,...', NULL, 29290000, 13, NULL, NULL, NULL, 0, NULL, NULL, NULL),
-(5, 2, 3, 'iPhone 11 64GB', 'Thời gian gần đây, thị trường smartphone đang có những bước chuyển mình mạnh mẽ với sự ra đời của hàng loạt những sản phẩm với các công nghệ hiện đại bậc nhất như: bộ vi xử lý Snapdragon 8 Gen 2, tấm nền màn hình QLED… Đó có thể là những bước tiến lớn nhưng nó không có nghĩa là dấu chấm hết cho những thiết bị đã được ra mắt trước đó như iPhone 11.\r\nMặc dù không còn giữ được sức nóng và độ hot như những ngày đầu mới được ra mắt nhưng ở thời điểm hiện tại, bạn khó có thể tìm được một sản phẩm nào sở hữu hiệu năng ổn định với mức giá thành siêu rẻ như iPhone 11. Với việc được hạ giá thành xuống thấp tới mức không thể nào hấp dẫn hơn, iPhone 11 sẽ tạo nên một con sốt trên thị trường smartphone thời điểm này.\r\n\r\nHiện tại, iPhone 11 vẫn được các chuyên gia đánh giá rất cao về độ ổn định của cấu hình trong thời gian dài sử dụng cũng như độ sắc nét mượt mà của màn hình trong các tác vụ như xem phim, chơi game. Về thiết kế bên ngoài, sản phẩm cũng vẫn luôn nhận được những phản hồi tích cực về sự bền bỉ, chắc chắn, cho cảm giác cầm nắm thoải mái, vừa tay.', NULL, 8650000, 10, NULL, NULL, NULL, 0, NULL, NULL, NULL),
-(6, 2, 1, 'iPhone 13 256GB', 'Hiệu năng bứt phá với con chip A17 Pro\r\nNhờ chủ động trong việc sản xuất chip mà các mẫu iPhone luôn sở hữu một hiệu năng vượt trội. Và iPhone 15 Pro Max 1TB cũng không phải là một ngoại lệ. Bởi mẫu điện thoại cao cấp nhất này sẽ được Apple trang bị thế hệ chip A17 Pro mới nhất để có được một hiệu năng mượt mà và đáng tin cậy.\r\nCon chip sử dụng tiến trình 3nm này sẽ giúp người dùng thực hiện mọi tác vụ khó khăn liên quan tới công việc và giải trí mà không gặp bất cứ trở ngại nào. Hệ thống cấu trục 6 lõi CPU cùng công nghệ Neutral Engine 16 lõi cho phép điện thoại có thể xử lý tới 35 ngàn tỷ tác vụ mỗi giây. Đồng thời chip A17 Pro còn được tái thiết kế GPU để mang tới hiệu năng đồ hoạ tốt nhất mà người dùng có thể tìm kiếm được trên các sản phẩm điện thoại ở thời điểm này.\r\n\r\nNhờ có thiết kế GPU mới này, trải nghiệm chơi game của người dùng sẽ được nâng lên một tầm cao mới. Môi trường và các nhân vật trong game sẽ có độ chi tiết, sống động. Đồng thời, chip A17 Pro vẫn sẽ đảm bảo khả năng tiết kiệm năng lượng hàng đầu nhờ được thừa hưởng từ thế hệ vi xử lý tiền nhiệm.', NULL, 42890000, 5, NULL, NULL, NULL, 1, NULL, NULL, NULL);
+INSERT INTO `products` (`id`, `brand_id`, `product_category_id`, `name`, `description`, `content`, `price`, `qty`, `discount`, `weight`, `sku`, `featured`, `tag`, `Views`, `img`, `created_at`, `updated_at`) VALUES
+(3, 2, 1, 'Iphone 13 128GB', 'Về kích thước, iPhone 13 sẽ có 4 phiên bản khác nhau và kích thước không đổi so với series iPhone 12 hiện tại. Nếu iPhone 12 có sự thay đổi trong thiết kế từ góc cạnh bo tròn (Thiết kế được duy trì từ thời iPhone 6 đến iPhone 11 Pro Max) sang thiết kế vuông vắn (đã từng có mặt trên iPhone 4 đến iPhone 5S, SE).\r\n\r\nThì trên điện thoại iPhone 13 vẫn được duy trì một thiết kế tương tự. Máy vẫn có phiên bản khung viền thép, một số phiên bản khung nhôm cùng mặt lưng kính. Tương tự năm ngoái, Apple cũng sẽ cho ra mắt 4 phiên bản là iPhone 13, 13 mini, 13 Pro và 13 Pro Max.', NULL, 13590000, 15, NULL, NULL, NULL, 0, NULL, 0, 'iphone13.webp', NULL, NULL),
+(4, 2, 1, 'iPhone 15 Pro Max 256GB', 'Hiệu năng mạnh mẽ cùng chipset A17 Pro tiên tiến \r\nSản phẩm trang bị chip A17 Pro được sản xuất trên tiến trình 3nm, mang lại bước cải tiến đột phá. Nhờ những cải tiến về thiết kế và kiến trúc vi mô, CPU mới nhanh hơn đến 10%, hỗ trợ tính năng như Giọng nói cá nhân hay tự động sửa lỗi chuyên nghiệp. Không chỉ vậy, GPU nhanh hơn đến 20% với thiết kế 6 lõi mới nhằm nâng cao hiệu suất và tiết kiệm năng lượng.\r\niPhone 15 Pro đã có một bước tiến đáng kể với công nghệ dò tia tăng tốc phần cứng, với tốc độ nhanh hơn gấp 4 lần so với các phiên bản trước đây dùng công nghệ dò tia dựa trên phần mềm. Kết quả của sự cải tiến này là trải nghiệm đồ họa trên thiết bị trở nên mượt mà hơn, đặc biệt là trải nghiệm khi chơi game và sử dụng ứng dụng thực tế ảo (AR) sẽ trở nên sống động hơn. Người dùng sẽ có trải nghiệm chiến game mượt mà hơn với: Resident Evil 4, Seadth Stranding, Resident Evil Village hay Assassin\'s Creed Mirage,...', NULL, 29290000, 13, NULL, NULL, NULL, 0, NULL, 0, 'iphone15.webp', NULL, NULL),
+(5, 2, 3, 'iPhone 11 64GB', 'Thời gian gần đây, thị trường smartphone đang có những bước chuyển mình mạnh mẽ với sự ra đời của hàng loạt những sản phẩm với các công nghệ hiện đại bậc nhất như: bộ vi xử lý Snapdragon 8 Gen 2, tấm nền màn hình QLED… Đó có thể là những bước tiến lớn nhưng nó không có nghĩa là dấu chấm hết cho những thiết bị đã được ra mắt trước đó như iPhone 11.\r\nMặc dù không còn giữ được sức nóng và độ hot như những ngày đầu mới được ra mắt nhưng ở thời điểm hiện tại, bạn khó có thể tìm được một sản phẩm nào sở hữu hiệu năng ổn định với mức giá thành siêu rẻ như iPhone 11. Với việc được hạ giá thành xuống thấp tới mức không thể nào hấp dẫn hơn, iPhone 11 sẽ tạo nên một con sốt trên thị trường smartphone thời điểm này.\r\n\r\nHiện tại, iPhone 11 vẫn được các chuyên gia đánh giá rất cao về độ ổn định của cấu hình trong thời gian dài sử dụng cũng như độ sắc nét mượt mà của màn hình trong các tác vụ như xem phim, chơi game. Về thiết kế bên ngoài, sản phẩm cũng vẫn luôn nhận được những phản hồi tích cực về sự bền bỉ, chắc chắn, cho cảm giác cầm nắm thoải mái, vừa tay.', NULL, 8650000, 10, NULL, NULL, NULL, 0, NULL, 0, 'iphone11.jpeg', NULL, NULL),
+(6, 2, 1, 'iPhone 13 256GB', 'Hiệu năng bứt phá với con chip A17 Pro\r\nNhờ chủ động trong việc sản xuất chip mà các mẫu iPhone luôn sở hữu một hiệu năng vượt trội. Và iPhone 15 Pro Max 1TB cũng không phải là một ngoại lệ. Bởi mẫu điện thoại cao cấp nhất này sẽ được Apple trang bị thế hệ chip A17 Pro mới nhất để có được một hiệu năng mượt mà và đáng tin cậy.\r\nCon chip sử dụng tiến trình 3nm này sẽ giúp người dùng thực hiện mọi tác vụ khó khăn liên quan tới công việc và giải trí mà không gặp bất cứ trở ngại nào. Hệ thống cấu trục 6 lõi CPU cùng công nghệ Neutral Engine 16 lõi cho phép điện thoại có thể xử lý tới 35 ngàn tỷ tác vụ mỗi giây. Đồng thời chip A17 Pro còn được tái thiết kế GPU để mang tới hiệu năng đồ hoạ tốt nhất mà người dùng có thể tìm kiếm được trên các sản phẩm điện thoại ở thời điểm này.\r\n\r\nNhờ có thiết kế GPU mới này, trải nghiệm chơi game của người dùng sẽ được nâng lên một tầm cao mới. Môi trường và các nhân vật trong game sẽ có độ chi tiết, sống động. Đồng thời, chip A17 Pro vẫn sẽ đảm bảo khả năng tiết kiệm năng lượng hàng đầu nhờ được thừa hưởng từ thế hệ vi xử lý tiền nhiệm.', NULL, 42890000, 5, NULL, NULL, NULL, 1, NULL, 0, 'iphone13.webp', NULL, NULL),
+(7, 4, 1, 'Nokia Lumia 520', 'Mặc dù Lumia 520 vẫn còn thiếu một số “phụ kiện” như đèn flash, máy ảnh phụ nhưng đó là một sản phẩm tuyệt vời với mức giá rẻ. Máy có một thiết kế đẹp, bắt mắt cùng cấu hình phải chăng với vi xử lý hai nhân. Nokia Lumia 520 thích hợp với nhiều đối tượng sử dụng đặc biệt là những người yêu thích Windows Phone, muốn trải nghiệm hệ điều hành mới mà không muốn bỏ ra nhiều chi phí', NULL, 2500000, 4, NULL, NULL, NULL, 1, NULL, 0, 'nokia520.jpeg', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -175,6 +178,14 @@ CREATE TABLE `product_details` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `product_details`
+--
+
+INSERT INTO `product_details` (`id`, `product_id`, `color`, `size`, `qty`, `created_at`, `updated_at`) VALUES
+(1, 5, 'Purple', 'L', 4, NULL, NULL),
+(2, 5, 'Red', 'L', 4, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -188,6 +199,14 @@ CREATE TABLE `product_images` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `product_images`
+--
+
+INSERT INTO `product_images` (`id`, `product_id`, `path`, `created_at`, `updated_at`) VALUES
+(1, 5, 'iphone11.png', NULL, NULL),
+(2, 4, 'iphone15.jpg', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -214,9 +233,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `avatar`, `level`, `description`, `created_at`, `updated_at`) VALUES
-(2, 'Admin', 'admin@gmail.com', NULL, 'admin', NULL, 'coding.png', 0, 'This is admin', NULL, NULL),
-(4, 'Bảo Ngọc', 'baongoc@gmail.com', NULL, '123456', NULL, NULL, 1, NULL, NULL, NULL),
-(5, 'blblafs', 'hahaha@gmail.com', NULL, '123456', NULL, 'bId04R.jpg', 1, NULL, NULL, NULL);
+(2, 'Admin', 'admin@gmail.com', NULL, 'admin', '03135e94bf07a5e7ad2b9b42e4f89855', 'coding.png', 0, 'This is admin', NULL, NULL),
+(3, 'Bảo Ngọc', 'baongoc@gmail.com', NULL, '123456', NULL, '402897278_2088784758120278_2873217156462964535_n.jpg', 1, NULL, NULL, NULL);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -311,7 +329,7 @@ ALTER TABLE `order_details`
 -- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT cho bảng `product_categories`
@@ -329,19 +347,19 @@ ALTER TABLE `product_comments`
 -- AUTO_INCREMENT cho bảng `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
