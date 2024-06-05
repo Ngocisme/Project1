eac75f51d79b79810bc6ba4fc40da998447117e6
vuanh12062004 <vuanh04052004@gmail.com>
2024-06-05 14:44:18 +0700

    Add cart and detail

--- views/users/includes/footer.php
+++ views/users/includes/footer.php
@@ -77,15 +77,15 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="../../assets/lib/easing/easing.min.js"></script>
-    <script src="../../assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="../../assets/users/lib/easing/easing.min.js"></script>
+    <script src="../../assets/users/lib/owlcarousel/owl.carousel.min.js"></script>
 
     <!-- Contact Javascript File -->
-    <script src="../../assets/mail/jqBootstrapValidation.min.js"></script>
-    <script src="../../assets/mail/contact.js"></script>
+    <script src="../../assets/users/mail/jqBootstrapValidation.min.js"></script>
+    <script src="../../assets/users/mail/contact.js"></script>
 
     <!-- Template Javascript -->
-    <script src="../../assets/js/main.js"></script>
+    <script src="../../assets/users/js/main.js"></script>
 </body>
 
 </html>
